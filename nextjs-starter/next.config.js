15ee4ff62b124dccd3d796c5c3774d200a01b480
SudGit10 <100399224+SudGit10@users.noreply.github.com>
2023-02-08 13:14:00 +0530

    Add changes

---
+++ nextjs-starter/next.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    trailingSlash: true,
+    async headers() {
+        return [
+            {
+              source: '/(.*)',
+              headers: [
+                {
+                  key: 'Content-Security-Policy',
+                  value:
+                    "default-src 'self'; font-src 'self' 'https://fonts.googleapis.com'; img-src 'self' *.somewhere.com; script-src 'self'",
+                },
+                {
+                  key: 'X-Frame-Optionshh',
+                  value: 'DENY',
+                },
+                {
+                  key: 'X-Content-Type-Options',
+                  value: 'nosniff',
+                },
+                {
+                  key: 'Referrer-Policy',
+                  value: 'origin-when-cross-origin',
+                },
+                {
+                  key: 'Permissions-Policy',
+                  value: "camera=(); battery=(self); geolocation=(); microphone=('https://somewhere.com')",
+                },
+              ],
+            },
+          ];
+      },
+};
